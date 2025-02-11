58678e2aac415bdade8d48ab1f773be6fdd37440
Adam Cole <adamthomascole@gmail.com>
2025-02-11 14:38:32 -0500

    Add change size functionality and dynamic div size

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 const gameContainer = document.querySelector("#gameContainer");
 const gameDiv = document.querySelector(".gameDiv");
-const divArray = [];
+let divArray = [];
 
 function getRandomColor() {
     const characters = '0123456789ABCDEF';
@@ -21,27 +21,59 @@ function changeColor() {
 }
 
 function buildDivArray(divNum) {
+    gameContainer.innerHTML = ""; 
+    
+    const width = 896;
+    const gapSize = 4;
+    const singleSide = (width / divNum);
+    const divSize = Math.floor(singleSide-gapSize)
+    console.log(divSize);
+
     for (let i = 0; i < divNum**2; i++) {
+        console.log(i);
         divArray[`div${i}`] = document.createElement("div");
         let newDiv = divArray[`div${i}`];
 
         divArray.push(newDiv);
 
         newDiv.classList.add('gameDiv');
-        newDiv.style.width = '24px';
-        newDiv.style.height = '24px';
-        newDiv.style.backgroundColor = 'black';
+        newDiv.style.width = divSize.toString() + "px";
+        newDiv.style.height = divSize.toString() + "px";
+        newDiv.style.backgroundColor = 'grey';
 
+        let gapNum = gapSize.toString();
+        let gapStr = gapNum + "px"
+
+        gameContainer.style.gap = gapStr
         gameContainer.appendChild(divArray[`div${i}`]);
     };
 
     for (const square of divArray) {
-        //square.style.margin = '12px';
         square.addEventListener('mouseover', changeColor);
-        console.log(square);
-    }
-    
+    };
+
+    gameContainer.style.width = `${width}px`
 };
 
-console.log(divArray)
+function newGrid() {
+    let input = prompt("How many squares wide and tall should your new grid be?", "16")
+    let inputNum = Number(input);
+    if (inputNum > 50) {
+        alert("Maximum size of 50x50!");
+    } else if (input !== null && input !== "") {
+        gameContainer.innerHTML = ""; 
+        divArray = [];
+        buildDivArray(input);
+    } else {
+        let input = prompt("Invalid Input!\nHow many squares wide and tall should your new grid be?", "16")
+        gameContainer.innerHTML = ""; 
+        divArray = [];
+        buildDivArray(input);
+    };
+};
+
 buildDivArray(16);
+
+
+
+
